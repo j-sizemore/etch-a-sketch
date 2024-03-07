256af6f8994480658fb117e398cac335d52254a4
Jacquelyn Sizemore <jacquelyn.sizemore@gmail.com>
2024-03-07 15:29:33 -0500

    Add ability to generate new grid

--- script.js
+++ script.js
@@ -1,32 +1,54 @@
-const GRID_SIZE = 16; // length and width of grid
-const CELL_SIZE = 50; // length and width in px of cell
+const MAX_WIDTH = 960.0; // maximum grid width in px
 const DRAW_COLOR = 'black';
 
-const container = document.createElement('div');
-container.id = 'container';
+function generateNewGrid(gridSize) {
+    const container = document.createElement('div');
+    container.id = 'container';
 
-for (let i = 0; i < GRID_SIZE; i++) {
-    let newRow = document.createElement('div');
-    newRow.className = 'grid-row';
-    for (let j = 0; j < GRID_SIZE; j++) {
-        let newCell = document.createElement('div');
-        newCell.className = 'grid-cell';
-        newCell.style.setProperty('height', CELL_SIZE + 'px');
-        newCell.style.setProperty('width', CELL_SIZE + 'px');
-        newRow.appendChild(newCell);
+    let cellSize = MAX_WIDTH / gridSize; // length and width in px of cell
+    
+    for (let i = 0; i < gridSize; i++) {
+        let newRow = document.createElement('div');
+        newRow.className = 'grid-row';
+        for (let j = 0; j < gridSize; j++) {
+            let newCell = document.createElement('div');
+            newCell.className = 'grid-cell';
+            newCell.style.setProperty('height', cellSize + 'px');
+            newCell.style.setProperty('width', cellSize + 'px');
+            newRow.appendChild(newCell);
+        }
+        container.appendChild(newRow);
     }
-    container.appendChild(newRow);
+    
+    document.body.appendChild(container);
+    
+    function changeBgColor(event) {
+        event.target.style.backgroundColor = DRAW_COLOR;
+        event.target.removeEventListener('mouseover', changeBgColor);
+    }
+    
+    const cells = document.querySelectorAll('.grid-cell');
+    
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', changeBgColor);
+    });
 }
 
-document.body.appendChild(container);
+generateNewGrid(16);
 
-function changeBgColor(event) {
-    event.target.style.backgroundColor = DRAW_COLOR;
-    event.target.removeEventListener('mouseover', changeBgColor);
-}
+const newGridButton = document.querySelector('#new-grid-button');
+newGridButton.addEventListener('click', (event) => {
+    let userGridSize = 101;
+    while(userGridSize > 100 || userGridSize < 1) {
+        let userInput = parseInt(prompt("Enter grid size (max 100):"));
+        if (!isNaN(userInput)) {
+            userGridSize = userInput;
+        }
+    }
 
-const cells = document.querySelectorAll('.grid-cell');
+    // remove current grid
+    document.body.removeChild(document.querySelector('#container'));
 
-cells.forEach((cell) => {
-    cell.addEventListener('mouseover', changeBgColor);
+    generateNewGrid(userGridSize);
 });
+
